b9e65d572e4c48917169910b6cffad5e71ae83fc
Shubham Kailas Deshmukh <87111500+shubhamdsk@users.noreply.github.com>
2022-01-09 10:58:51 +0530

    Delete Assignment 2 directory

--- Assignment 2/Assignment 2/P_1_Append_Item_in_List.py
+++
@@ -1,9 +0,0 @@
-# 1. Write a Python program to append items from a specified list.
-
-from array import *
-num_list = [1, 2, 6, -8]
-array_num = array('i', [])
-print("Items in the list: " + str(num_list))
-print("Append items from the list: ")
-array_num.fromlist(num_list)
-print("Items in the array: "+str(array_num))
