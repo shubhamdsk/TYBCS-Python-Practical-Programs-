0c2afd86c26a87d4c6641b6a76b8e8dbfeeaef2f
Shubham Kailas Deshmukh <87111500+shubhamdsk@users.noreply.github.com>
2022-01-09 11:27:24 +0530

    Assignment 2

--- Assignment 2/P_4_SnakeCase_to_PascalCase.py
+++ Assignment 2/P_4_SnakeCase_to_PascalCase.py
@@ -5,11 +5,8 @@ test_str = input("Enter String :")
 # printing original string
 print("The original string is : " + test_str)
 
-# Convert Snake case to Pascal case
-# Using title() + replace()
 res = test_str.replace("_", " ").title().replace(" ", "")
 
-# printing result
 print("The String after changing case : " + str(res))
 
 """
