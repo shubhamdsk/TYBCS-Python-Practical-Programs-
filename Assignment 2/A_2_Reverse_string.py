3169cae4e75988da8b355f74628033976290636a
Shubham Kailas Deshmukh <87111500+shubhamdsk@users.noreply.github.com>
2022-01-09 11:05:33 +0530

    Delete Assignment 2 directory

--- Assignment 2/A_2_Reverse_string.py
+++
@@ -1,9 +0,0 @@
-# 2) Write a python program to Reverse words in a given String
-
-string = (input("Enter String :"))
-
-words = string.split()
-
-words = list(reversed(words))
-
-print(" ".join(words))
--- Assignment 2/P_3_Frequency_of_Word_in_String.py
+++
@@ -1,9 +0,0 @@
-# 3. Write a python program Words Frequency in String Shorthands
-
-string = input("Enter String :")
-print("\nEntered String :", string)
-
-word = {key: string.count(key) for key in string.split()}
-
-print("\nWords in the string :")
-print(word)
