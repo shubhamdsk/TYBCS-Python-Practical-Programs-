823f3c7ff72978ba10911f411ba05e0cb90ef4fa
Shubham Kailas Deshmukh <87111500+shubhamdsk@users.noreply.github.com>
2022-01-12 19:56:20 +0530

    Assignment 3

---
+++ Assignment 3/A_D_3_Iterate_Dict.py
@@ -0,0 +1,11 @@
+# 3) Write a Python program to iterate over dictionaries using for loops.
+
+d = {'Red': 1, 'Green': 2, 'Blue': 3} 
+for color_key, value in d.items():
+     print(color_key, 'corresponds to ', d[color_key]) 
+
+"""
+Red corresponds to  1
+Green corresponds to  2
+Blue corresponds to  3
+"""
