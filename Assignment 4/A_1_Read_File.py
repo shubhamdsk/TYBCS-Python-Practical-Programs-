ed2b56f1d9671741da5a2676347536c500f97ca5
Shubham Kailas Deshmukh <87111500+shubhamdsk@users.noreply.github.com>
2022-01-14 21:58:44 +0530

    Assignment 4

--- Assignment 4/A_1_Read_File.py
+++ Assignment 4/A_1_Read_File.py
@@ -1,7 +1,7 @@
 # 1) Write a Python program to read an entire text file.
 
-def file_read(fname):
-    txt = open(fname)
+def file_read(A_1_Test):
+    txt = open(A_1_Test)
     print(txt.read())
 
 file_read("A_1_Test.txt")
